ac81012a3f14782a6bb05a9a2eee3976efefa447
tonyshu <tonynshu@gmail.com>
2023-08-31 14:13:20 +0800

    completed lab???

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
+        int age;
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.println("Enter your age (only integers accepted):");
+        age = ageScanner.nextInt();
+        System.out.flush();
+        double ageDouble = age;
+        double shouldDate = (ageDouble/2)+7;
+        System.out.println("Someone who is " + age + " years old should date someone who is at least " + shouldDate + "years old");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        int age;
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.println("Enter your age (only integers accepted):");
+        age = ageScanner.nextInt();
+        System.out.flush();
+        int shouldRead = 100 - age;
+        System.out.println("Someone who is " + age + " years old should read at least " + shouldRead + " pages before giving up on a book.");
+
+
 
     }
 }
